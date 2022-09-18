77ed36b3c98caaec392bd78918fb25a4e58172ef
Heru Gunawan <herugunawan791@gmail.com>
2022-09-18 21:06:26 +0700

    memperbaharui code yang salah

--- challenge-promises-eeruxyz-main/promise.js
+++ challenge-promises-eeruxyz-main/promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async emosi => {
+      try {
+        const theaterixx = await promiseTheaterIXX();
+        const theatervgc = await promiseTheaterVGC();
+
+        const hasil = theaterixx
+            .map(item => item.hasil)
+            .concat(theatervgc.map(item => item.hasil));
+
+        emosi = hasil.filter(hasil => hasil === emosi);
+
+        return emosi.length;
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 module.exports = {
   promiseOutput,
